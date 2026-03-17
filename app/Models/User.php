c30d974a94c30beb3752fe1d690b3757dc989bf1
Yassine Sekkah <yassinesekkah11@gmail.com>
2026-03-17 13:18:44 +0000

    feat: BA-57 add store methode to create restaurant

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -55,5 +56,10 @@ class User extends Authenticatable
     public function plats(): HasMany
     {
         return $this->hasMany(Plat::class);
+    }
+
+    public function restaurant(): HasOne
+    {
+        return $this->hasOne(Restaurant::class);
     }
 }
